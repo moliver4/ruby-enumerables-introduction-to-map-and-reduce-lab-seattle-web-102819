1a0c86a89812468b4c7f621aa173b2c11862c15e
Tiffany Tang <tifftang22@gmail.com>
2019-10-03 16:17:15 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -6,33 +6,3 @@ def map_to_negativize(source_array)
     counter++
   end
 end
-
-def map_to_no_change(source_array)
-  new_array = Array.new
-  counter = 0
-  while counter<source_array.length do
-    new_array[counter] = source_array[counter]
-    counter++
-  end
-end
-
-def map_to_double(source_array)
-  new_array = Array.new
-  counter = 0
-  while counter<source_array.length do
-    new_array[counter] = source_array[counter]
-    counter++
-  end
-end
-
-def map_to_square
-end
-
-def reduce_to_total
-end
-
-def reduce_to_all_true
-end
-
-def reduce_to_any_true
-end
