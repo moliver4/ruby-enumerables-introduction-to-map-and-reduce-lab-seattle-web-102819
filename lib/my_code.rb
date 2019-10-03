3353727719306b4510e633080e93755e7b5c2fc5
Tiffany Tang <tifftang22@gmail.com>
2019-10-03 16:25:02 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -2,7 +2,7 @@ def map_to_negativize(source_array)
   new_array = Array.new
   counter = 0
   while counter < source_array.length do
-    new_array[counter]=source_array[counter]* -1
+    new_array << source_array[counter]* -1
     counter++
   end
   new_array
